34c5c490fd302a273a2ef5c322591935f14974b5
haciarpaci <haciarpaci12@gmail.com>
2021-08-14 18:33:41 +0300

    updated pomxml lastUpsates1

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -4,11 +4,15 @@ import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
+import java.util.concurrent.TimeUnit;
+
 public class Hooks {
     @Before
     public void setUp(){
 
         System.out.println("\t  this comes from BEFORE");
+        Driver.get().manage().window().maximize();
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
     @After
     public void tearDown(){
